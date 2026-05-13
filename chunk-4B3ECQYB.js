c140c29ca495db94ecda700940be4381ad291976
zafarluni <zafarluni@users.noreply.github.com>
2026-05-13 17:57:15 +0000

    deploy: zafarluni/ayla-home@d4cb85af31b2b4445c6084b2552b93f95cdbaef6

---
+++ chunk-4B3ECQYB.js
@@ -0,0 +1,1 @@
+var a={production:!0,appUrl:"https://zafarluni.github.io/ayla",supabaseUrl:"https://vsrehalhipmeegxgfmdy.supabase.co",supabaseKey:"sb_publishable_hz71X_7toxukuCnxllgR0w_QN63BThz"};export{a};
